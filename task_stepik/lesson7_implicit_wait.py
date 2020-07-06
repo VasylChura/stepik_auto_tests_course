84f929c3dedcd5555c67a900e9477bf592e0967b
Vasyl Chura <vasil.chura@gmail.com>
2020-07-06 16:36:53 +0300

    new method added in implicity_wait

--- task_stepik/lesson7_implicit_wait.py
+++ task_stepik/lesson7_implicit_wait.py
@@ -1,5 +1,14 @@
+
 from selenium import webdriver
 
 browser = webdriver.Chrome("C:\\Chromedriver\\chromedriver")
-browser.get("http://suninjuly.github.io/cats.html")
-b = browser.find_element_by_id("button")
+# говорим WebDriver искать каждый элемент в течение 5 секунд
+browser.implicitly_wait(5)
+
+browser.get("http://suninjuly.github.io/wait1.html")
+
+button = browser.find_element_by_id("verify")
+button.click()
+message = browser.find_element_by_id("verify_message")
+
+assert "successful" in message.text
